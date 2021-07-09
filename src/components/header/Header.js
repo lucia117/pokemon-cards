5c734f5d99ff432029eb30cfdbe8cf748e78482f
luciascharff <luciascharff@gmail.com>
2021-07-09 20:14:10 -0300

    created header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,8 @@
 const Header = () => (
-    <h2>Header</h2>
+    <div className="app__header">
+        <a href="https://fontmeme.com/es/fuente-pokemon/">
+            <img src="https://fontmeme.com/permalink/210710/8c55579aa8bb046f88cd4a4d60acdc19.png" alt="fuente-pokemon" border="0"/>
+        </a>
+    </div>
 );
-
 export default Header
